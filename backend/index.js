d1e650a352fd67ea1cbcb21c7dd3c91ad6cc6b61
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-06-11 18:06:49 +0600

    fix environment variable

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,9 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://bdtuitions.vercel.app",
+      "https://bdtutionsf.vercel.app",
       "https://cerulean-maamoul-f79e46.netlify.app",
-      "https://preeminent-mermaid-04ba7b.netlify.app/dashboard",
+      "https://preeminent-mermaid-04ba7b.netlify.app",
     ],
   }),
 );
